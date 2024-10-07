6f88ae1c5827db590b78dbbfb3ad8cdd92545c7e
Agneya48 <josh.t.hampton@gmail.com>
2024-10-06 20:49:06 -0400

    Completed Task 1, Fixed Typos

--- src/CtoFConverter.java
+++ src/CtoFConverter.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 public class CtoFConverter {
     public static void main(String[] args) {
 
-        double cTemp = 0.0; //temperature in Celcius to be entered
-        double fTemp = 0.0; //teperature in Farenheit that will be calculated
+        double cTemp = 0.0; //temperature in Celsius to be entered
+        double fTemp = 0.0; //teperature in Fahrenheit that will be calculated
         boolean done = false;
         String trash = "";
         String continueYN = "";
@@ -12,12 +12,12 @@ public class CtoFConverter {
         try (Scanner in = new Scanner(System.in)) { // try auto-closes Scanner so there's no memory leak
 
             do {
-                System.out.print("\nEnter a temperature value in Celcius: ");
+                System.out.print("\nEnter a temperature value in Celsius: ");
                 if(in.hasNextDouble()) { //safe to read in a double
                     cTemp = in.nextDouble();
                     in.nextLine(); //clears the newline from buffer
                     fTemp = 1.8 * cTemp + 32;
-                    System.out.printf("%n%.2f degrees Celcius is equal to %.2f degrees Farenheit.", cTemp, fTemp);
+                    System.out.printf("%.2f degrees Celsius is equal to %.2f degrees Fahrenheit.", cTemp, fTemp);
                     //rounding double values to 2 decimal places to avoid occasional odd double value displays
                     System.out.print("\nWould you like to continue? Y/N: ");
                     continueYN = in.nextLine();
@@ -28,7 +28,7 @@ public class CtoFConverter {
                 }
                 else { //not a double so use nextLine instead to read a String
                     trash = in.nextLine();
-                    System.out.println("\nInvalid input. Please enter a number instead.");
+                    System.out.println("Invalid input. Please enter a number instead.");
                 }
             } while(!done);
         }
