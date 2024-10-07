1a9f9428530f320a7a51a8f03306e9702ab76687
Agneya48 <josh.t.hampton@gmail.com>
2024-10-06 20:40:48 -0400

    Completed Task 1

--- src/CtoFConverter.java
+++ src/CtoFConverter.java
@@ -1,5 +1,36 @@
-public class Main {
+import java.util.Scanner;
+
+public class CtoFConverter {
     public static void main(String[] args) {
-        
+
+        double cTemp = 0.0; //temperature in Celcius to be entered
+        double fTemp = 0.0; //teperature in Farenheit that will be calculated
+        boolean done = false;
+        String trash = "";
+        String continueYN = "";
+
+        try (Scanner in = new Scanner(System.in)) { // try auto-closes Scanner so there's no memory leak
+
+            do {
+                System.out.print("\nEnter a temperature value in Celcius: ");
+                if(in.hasNextDouble()) { //safe to read in a double
+                    cTemp = in.nextDouble();
+                    in.nextLine(); //clears the newline from buffer
+                    fTemp = 1.8 * cTemp + 32;
+                    System.out.printf("%n%.2f degrees Celcius is equal to %.2f degrees Farenheit.", cTemp, fTemp);
+                    //rounding double values to 2 decimal places to avoid occasional odd double value displays
+                    System.out.print("\nWould you like to continue? Y/N: ");
+                    continueYN = in.nextLine();
+                    if(continueYN.equalsIgnoreCase("Y"))
+                        done = false; //ask for another input if user says they want to continue
+                    else // typo or user saying they don't want to continue will terminate input loop
+                        done = true;
+                }
+                else { //not a double so use nextLine instead to read a String
+                    trash = in.nextLine();
+                    System.out.println("\nInvalid input. Please enter a number instead.");
+                }
+            } while(!done);
+        }
     }
 }
